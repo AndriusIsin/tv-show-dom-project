43bf31a5aa96c14d2e28de919c4e099fbd75f536
AndriusIsin <isin.andrius@gmail.com>
2023-05-08 20:36:21 +0100

    200 done

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 //You can edit ALL of the code here
+let selectElement = document.getElementById("choose");
+let searchElement = document.getElementById("search-input");
+let numberOfEpisodes = document.getElementById("episodes-number");
 let rootElem = document.getElementById("root");
+const allEpisodes = getAllEpisodes();
 function setup() {
-  const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
+  addOptions();
+  numberOfEpisodes.innerText = allEpisodes.length;
+  searchEpisodes();
+  chooseAnEpisode();
 }
-
+// level 100
 function makePageForEpisodes(episodeList) {
   episodeList.forEach((episode) => addEpisode(episode));
+  numberOfEpisodes.innerText = episodeList.length;
 }
 function addEpisode(episode) {
   let episodeContainer = document.createElement("div");
@@ -27,6 +35,41 @@ function addEpisode(episode) {
   episodeContainer.append(title, imgEpisode, summaryText);
   rootElem.append(episodeContainer);
 }
+// level 200
+function searchEpisodes() {
+  searchElement.addEventListener("input", (event) => {
+    const inputValue = event.target.value.toLowerCase();
+    rootElem.textContent = "";
 
-setup();
+    const filteredEpisodes = allEpisodes.filter((episode) => {
+      return (
+        episode.summary.toLowerCase().includes(inputValue.toLowerCase()) ||
+        episode.name.toLowerCase().includes(inputValue.toLowerCase())
+      );
+    });
+    makePageForEpisodes(filteredEpisodes);
+  });
+}
+// level 300
+function addOptions() {
+  allEpisodes.forEach((episode) => {
+    let optionElement = document.createElement("option");
+    optionElement.innerHTML = `S${episode.season
+      .toString()
+      .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${
+      episode.name
+    }`;
+    selectElement.appendChild(optionElement);
+  });
+}
+function chooseAnEpisode() {
+  selectElement.addEventListener("change", (event) => {
+    const inputValue = event.target.value;
+    rootElem.textContent = "";
+    const filteredEpisodes = allEpisodes.filter((episode) =>
+      inputValue.includes(episode.name)
+    );
+    makePageForEpisodes(filteredEpisodes);
+  });
+}
 window.onload = setup;
